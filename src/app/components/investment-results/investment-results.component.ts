c7e52917bc98471d16594190fb8bbc1b2690dcd0
Olga Volobueva <olvik1605@gmail.com>
2025-04-02 19:03:25 -0400

    update: added CurrencyPipe for InvestmentResultsComponent

--- src/app/components/investment-results/investment-results.component.ts
+++ src/app/components/investment-results/investment-results.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { InvestmentResult } from './investment-result.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
