fb1fa09b4bc1b7dde356cef4044f1f78928753d6
Olga Volobueva <olvik1605@gmail.com>
2025-04-01 21:26:28 -0400

    feat: added calculateInvestmentResults

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { UserInputComponent } from "./components/user-input/user-input.component";
+import { HeaderComponent } from './components/header/header.component';
+import { UserInputComponent } from './components/user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +11,30 @@ import { UserInputComponent } from "./components/user-input/user-input.component
 })
 export class AppComponent {
   title = 'investment-calculator';
+  calculateInvestmentResults(data: {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }) {
+    const { initialInvestment, duration, annualInvestment, expectedReturn } = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+    return annualData;
+  }
 }
